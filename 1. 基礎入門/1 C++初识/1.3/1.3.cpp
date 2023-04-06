664e9cc6f59dac6723c9ae31ed5d1268e61fe2cb
chioujryu <chioujryu@gmail.com>
2023-04-06 19:50:53 +0800

    2023.04.06

---
+++ 1. 基礎入門/1 C++初识/1.3/1.3.cpp
@@ -0,0 +1,21 @@
+// 作用：给一段指定的内存空间起名，方便操作这段内存
+// 语法：数据类型 变量名 = 初始值;
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	//变量的定义
+	//语法：数据类型  变量名 = 初始值
+    //注意：C++在创建变量时，必须给变量一个初始值，否则会报错
+
+	int a = 10;
+
+	cout << "a = " << a << endl;
+	
+	system("pause");
+
+	return 0;
+}
